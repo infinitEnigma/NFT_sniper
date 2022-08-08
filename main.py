31dab87159522bd6993f17f8316686f0db0352f8
Jovan Stojanovic <yole71@gmail.com>
2022-08-08 13:11:09 +0000

    minor fixes

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class PriceGetter:
             print("PriceGetter: something went wrong", e)
             return ''
         try:
-            sleep(0.5)
+            sleep(0.2)
             return self.driver.find_element(By.CSS_SELECTOR, 'body').text
         except Exception as e:
             print("PriceGetter: no ada_value", e)
@@ -82,9 +82,9 @@ class CheckFloors:
             embed1.set_thumbnail(url=ic_url[0])
             embed.append(embed1)
             for it in chgs: 
-              t = f'{chgs.index(it)+1}. {it[0]}:\n... new floor ₳ {it[1]}'
-              l = f'\n[NFT link - jpg.store]({nfts_urls[it[0]]})'
-              d = 'change: ' + str('₳ '+ str(it[2]) if it[2]<0 else f"₳ +{it[2]}") + '\n' + l
+              t = f'{chgs.index(it)+1}. {it[0]}:\n    ... new floor: ₳ {it[1]}'
+              l = f'\n    [NFT link - jpg.store]({nfts_urls[it[0]]})'
+              d = '    change: ' + str('₳ '+ str(it[2]) if it[2]<0 else f"₳ +{it[2]}") + '\n' + l
               embed1 = discord.Embed(title=t, description=d, color=1157128 if it[2]<0 else 16711680)
               for img in imgs:
                 if it[0] == img[0]: 
@@ -93,7 +93,7 @@ class CheckFloors:
               embed.append(embed1)
             embed2 = discord.Embed(title=coll_name, description="*collection current floors:*")
             for item in nfts_floors.items(): 
-              embed2.add_field(name=item[0], value='₳ ' + item[1], inline=True) 
+              embed2.add_field(name=item[0], value='₳ ' + str(item[1]), inline=True) 
             embed2.set_footer(text=f'Collection Total: ₳ {sum(nfts_floors.values())}')
             embed2.set_thumbnail(url=ic_url[0])
             embed.append(embed2)
@@ -129,7 +129,7 @@ async def on_ready():
         print("main: driver error:", e)
         sleep(3)
         #main()
-        await client.close()
+        #await client.close()
         return
     coll_urls, check_floors = [], {}
     global coll_floors
@@ -147,14 +147,14 @@ async def on_ready():
             print("main: no check_floors")
             sleep(3)
             #main()
-            await client.close()
+            #await client.close()
             return
         if -check_floors[coll[0]][1][0]+check_floors[coll[0]][1][1] > 0:
             for e in embed:
               await chnl.send(embed=e)
               await asyncio.sleep(0.1)
             quote = get_quote()
-            await chnl.send(f'||{quote}||')
+            if quote: await chnl.send(f'||{quote}||')
             print('discord message sent..\n\n')
     driver.quit()
     sleep(34)
@@ -172,7 +172,7 @@ async def on_ready():
                 print("main: no check_floors in while loop")
                 sleep(3)
                 #main()
-                await client.close()
+                #await client.close()
                 return
             # if there's a change in price send discord messages
             if -check_floors[coll[0]][1][0]+check_floors[coll[0]][1][1] > 0:
@@ -181,7 +181,7 @@ async def on_ready():
                     await chnl.send(embed=e)
                     await asyncio.sleep(0.1)
                 quote = get_quote()
-                await chnl.send(f'||{quote}||')
+                if quote: await chnl.send(f'||{quote}||')
                 print('discord message sent..\n\n')
             print(f'changes since\nthe session start: lowered {change_tracker[coll[0]][0]}, raised +{change_tracker[coll[0]][1]}')
             print(f'\nchanges/checks: {change_tracker[coll[0]][2]}/{GCounter}')
@@ -192,10 +192,13 @@ async def on_ready():
         sleep(34)
       
 def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = f"*{json_data[0]['q']}* - {json_data[0]['a']}"
-  return(quote)
+    try:
+        response = requests.get("https://zenquotes.io/api/random")
+        json_data = json.loads(response.text)
+    except:
+        print('no quote!')
+        return
+    return f"*{json_data[0]['q']}* - {json_data[0]['a']}"
 
 def openChrome():   
   chrome_options = Options()
@@ -204,16 +207,12 @@ def openChrome():
   chrome_options.add_argument('--headless')
   return webdriver.Chrome(options=chrome_options)
 
-#coll_floors = []
 
-def main():
-  global client
-  while True:
-    client.run(os.getenv('TOKEN'))
-    client = discord.Client()
+def main():  
+  client.run(os.getenv('TOKEN'))
+  
     
 if __name__ == "__main__":
-  keep_alive()
   main()
-  #client = None
-    
+  keep_alive()  
+  
