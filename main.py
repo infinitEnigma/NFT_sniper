d34129bab5a9b2fca4ca7c146639cb9f79943e4d
Jovan Stojanovic <yole71@gmail.com>
2022-08-11 22:26:39 +0000

    minor fixes

--- main.py
+++ main.py
@@ -49,59 +49,61 @@ class CheckFloors:
         self.raised = []
         self.lowered = []
     # extract prices and check for the changes
-    def checkPrices(self, coll_name, nfts_names, nfts_urls, nfts_floors):
+    def checkPrices(self, coll_name, nfts_nu, nfts_floors):
         print(f'Collection: {coll_name}\n')
         chgs = []
         quote = ''
-        for a in nfts_urls.values():
-            adavalue = PageGetter(self.driver).getPage(a)
-            if adavalue and adavalue != '':
-                adavalue = adavalue.split('\n')
-                for v in range(len(adavalue)-1):
-                    if adavalue[v] in nfts_names:
-                        if float(adavalue[v+1]) == nfts_floors[adavalue[v]]:
-                            print(f"{adavalue[v]}: {(30-len(adavalue[v]))*' '}{adavalue[v+1]} - no changes")
+        for a in nfts_nu[1].values():
+            bt = PageGetter(self.driver).getPage(a)
+            if bt and bt != '':
+                bt = bt.split('\n')
+                for v in range(len(bt)-1):
+                    if bt[v] in nfts_nu[0]:
+                        if float(bt[v+1]) == nfts_floors[bt[v]]:
+                            print(f"{bt[v]}: {(30-len(bt[v]))*' '}{bt[v+1]} - no changes")
                         else:
-                            change = float(adavalue[v+1])-nfts_floors[adavalue[v]]
-                            if float(adavalue[v+1]) > nfts_floors[adavalue[v]]:
-                                self.raised.append(f"{adavalue[v]},{adavalue[v+1]},{change}")
-                                print(f"{adavalue[v]}: {(30-len(adavalue[v]))*' '}{adavalue[v+1]} - floor raised: +{change}")
+                            change = float(bt[v+1])-nfts_floors[bt[v]]
+                            if float(bt[v+1]) > nfts_floors[bt[v]]:
+                                self.raised.append(f"{bt[v]},{bt[v+1]},{change}")
+                                print(f"{bt[v]}: {(30-len(bt[v]))*' '}{bt[v+1]} - floor raised: +{change}")
 
                             else:
-                                self.lowered.append(f"{adavalue[v]},{adavalue[v+1]},{change}")
-                                print(f"{adavalue[v]}: {(30-len(adavalue[v]))*' '}{adavalue[v+1]} - floor lowered!!! {change}")
-                            nfts_floors[adavalue[v]] = float(adavalue[v+1])
-                            chgs.append((adavalue[v], adavalue[v+1], round(change,2)))
+                                self.lowered.append(f"{bt[v]},{bt[v+1]},{change}")
+                                print(f"{bt[v]}: {(30-len(bt[v]))*' '}{bt[v+1]} - floor lowered!!! {change}")
+                            nfts_floors[bt[v]] = float(bt[v+1])
+                            chgs.append((bt[v], bt[v+1], round(change,2)))
                             quote = get_quote(self.driver)
                         break
             else:
-                print('CheckFloors: no adavalue')
+                print('CheckFloors: no body text')
                 return [0]
         print("\nTOTAL:", sum(nfts_floors.values()),"\n")
         embed = []
         # if change is detected prepare reports
         if len(self.raised)+len(self.lowered)>0:
             imgs = db['data'][coll_name][4]
-            # write last known values to file/db
+            # write last known values to db
             db[db['data'][coll_name][0]] = dict({it[0]: float(it[1]) for it in nfts_floors.items()})
             # prepare discord message - embed
             ic_url = [ic[1][3] for ic in db['data'].items() if ic[0]==coll_name]
             l = f'[Collection link - jpg.store]({db["data"][coll_name][1][:-12]})'
             d = l + "\n*collection latest changes:*\n" if len(chgs)>1 else l + '\n*collection latest change:*\n'
+            dt = f"\nCollection Total:  ₳ {sum(nfts_floors.values())} {'for common plots & condo' if 'Land' in coll_name else ''}"
             embed, twitt = [], []
             embed1 = discord.Embed(title=coll_name, description=d, color=1127128)
             embed1.set_thumbnail(url=ic_url[0])
             embed.append(embed1)
-            twitt.append([ic_url[0], coll_name])
+            twitt.append([ic_url[0], f'{coll_name}\nCollection link: {db["data"][coll_name][1][:-12]}{dt}'])
             for it in chgs:
                 t = f'{chgs.index(it)+1}. {it[0]}:\n... new floor: ₳ {it[1]}'
-                l = f'\n[NFT link - jpg.store]({nfts_urls[it[0]]})'
+                l = f'\n[NFT link - jpg.store]({nfts_nu[1][it[0]]})'
                 d = 'change: ' + str('₳ '+ str(it[2]) if it[2]<0 else f"₳ +{it[2]}") + '\n' + l
+                dt = '\nchange: ' + str('₳ '+ str(it[2]) if it[2]<0 else f"₳ +{it[2]}")
                 embed1 = discord.Embed(title=t, description=d, color=1157128 if it[2]<0 else 16711680)
                 for img in imgs:
                     if it[0] == img[0]:
                         embed1.set_thumbnail(url=img[1])
-                        twitt.append([img[1], t])
+                        twitt.append([img[1], f'{t}\n{dt}\nNFT link: {nfts_nu[1][it[0]]}'])
                         break
                 embed.append(embed1)
             embed2 = discord.Embed(title=coll_name, description="*collection current floors:*")
@@ -111,11 +113,11 @@ class CheckFloors:
             embed2.set_thumbnail(url=ic_url[0])
             embed.append(embed2)
             db['twitter'] = twitt
-            #twitt.append(ic_url[0], '*collection current floors:*')
+            print()
         # get change sums
-        sum_lowered = sum(float(s.split(',')[2]) for s in self.lowered)
-        sum_raised = sum(float(s.split(',')[2]) for s in self.raised)
-        return [nfts_floors, [sum_lowered, sum_raised], [embed, quote]]
+        db['sum_lowered'] = sum(float(s.split(',')[2]) for s in self.lowered)
+        db['sum_raised'] = sum(float(s.split(',')[2]) for s in self.raised)
+        return [-db['sum_lowered']+db['sum_raised'], [embed, quote]]
 
 
 @client.event
@@ -134,38 +136,38 @@ async def on_ready():
         print("main1: driver error:", e)
         sleep(1)
         return
-    check_floors = {}  
+    #check_floors = {}  
     change_tracker = dict({coll:[0,0,0] for coll in db['data'].keys()})
     GCounter = 0
     c = 0
     # loop through the collections and check floors
     while True:
         GCounter += 1
+        check = 'False'
         print('restarts:', db['restarts'])
         for coll in db['data'].items():
             # check floors
-            check_floors[coll[0]] = CheckFloors(driver).checkPrices(coll[0], db['coll_urls'][c][0], db['coll_urls'][c][1], db['coll_floors'][c])
-            #sleep(0.5)
-            if len(check_floors[coll[0]])<=1:
+            check = CheckFloors(driver).checkPrices(coll[0], db['coll_urls'][c], db['coll_floors'][c])
+            sleep(0.5)
+            if check == 'False' or check == [0]:
                 driver.quit()
                 print("main2: no check_floors in while loop")
                 sleep(3)
                 return
             # if there's a change in price 
             # update changes tracker and send discord messages
-            if -check_floors[coll[0]][1][0]+check_floors[coll[0]][1][1] > 0:
-                p = change_tracker[coll[0]][0]+check_floors[coll[0]][1][0]
-                n = change_tracker[coll[0]][1]+check_floors[coll[0]][1][1]
+            elif check[0] > 0:
+                p = change_tracker[coll[0]][0]+db['sum_lowered']
+                n = change_tracker[coll[0]][1]+db['sum_raised']
                 change_tracker[coll[0]] = [p, n, change_tracker[coll[0]][2]+1]
-                e, q = check_floors[coll[0]][2][0], check_floors[coll[0]][2][1] 
-                await sendDiscordMsgs(e, q, chnl, driver)
+                await sendDiscordMsgs(check[1][0], check[1][1], chnl)
             # print changes    
             print(f'changes since\nthe session start: lowered {change_tracker[coll[0]][0]}, raised +{change_tracker[coll[0]][1]}')
             print(f'\nchanges/checks: {change_tracker[coll[0]][2]}/{GCounter}\n')
             c += 1
         c = 0
         print(datetime.now(), 'pause...\n\n')
-        sleep(5)
+        sleep(10)
 
 
 def get_quote(driver):
@@ -174,7 +176,7 @@ def get_quote(driver):
     return f"*{json_data[0]['q']}* - {json_data[0]['a']}"
 
 
-async def sendDiscordMsgs(embeds, quote, chnl, driver):
+async def sendDiscordMsgs(embeds, quote, chnl):
     for e in embeds:
         await asyncio.sleep(0.2)
         await chnl.send(embed=e)
