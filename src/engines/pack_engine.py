3da88c02f5d48621cb9b4efedd0a272d87fee0d3
Jovan Stojanovic <yole71@gmail.com>
2022-08-25 23:17:50 +0000

    minor change

--- src/engines/pack_engine.py
+++ src/engines/pack_engine.py
@@ -11,8 +11,7 @@ def prepareMessages(coll_name, nfts_nu, nfts_floors, chgs):
     d = l + "\n*collection latest changes:*\n" if len(chgs)>1 else l + '\n*collection latest change:*\n'
     dt = f"\nCollection Total:  ₳ {coll_sum} (${coll_sumD})\n{'for common plots & condo' if 'Land' in coll_name else ''}"
     embed, twitt, sheet = [], [], []
-    embed1 = [coll_name, d, 1127128, imgs_d[0]]
-    embed.append(embed1)
+    embed.append([coll_name, d, 1127128, imgs_d[0]])
     twitt.append([ic_url[0], f'{coll_name}\nCollection link: {db["data"][coll_name][1][:-12]}{dt}'])
     for it in chgs:
         nft_priceD = round(float(it[1]) * db['adaprice'],2)
@@ -21,14 +20,13 @@ def prepareMessages(coll_name, nfts_nu, nfts_floors, chgs):
         d = 'change: ' + str('₳ '+ str(it[2]) + ' :arrow_lower_right:' if it[2]<0 else f"₳ +{it[2]} :arrow_upper_right:") + '\n' + l
         dollarP = 'usd price obtained via coingecko.com api'
         dt = f"{dollarP}\nchange: ₳ {it[2]} \u2198" if it[2]<0 else f"{dollarP}\nchange: ₳ +{it[2]} \u2197"
-        embed1 = [t, d, 1157128 if it[2]<0 else 16711680]
         for img in imgs:
             if it[0] == img[0]:
-                embed1.append(imgs_d[1][it[0]])
                 twitt.append([img[1], f'{t}\n{dt}\nNFT link: {nfts_nu[1][it[0]]}'])
-                sheet.append([coll_name, coll_sum,  db["data"][coll_name][1][:-12], ic_url[0], it[0], it[1], nfts_nu[1][it[0]], img[1]])
+                sheet.append([coll_name, coll_sum,  db["data"][coll_name][1][:-12], 
+                              ic_url[0], it[0], it[1], nfts_nu[1][it[0]], img[1]])
                 break
-        embed.append(embed1)
+        embed.append([t, d, 1157128 if it[2]<0 else 16711680, imgs_d[1][it[0]]])
     embed2 = [coll_name, "*collection current floors:*"]
     for item in nfts_floors.items():
         embed2.append([item[0], '₳ ' + str(item[1]), True])
